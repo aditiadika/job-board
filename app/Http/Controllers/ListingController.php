80094b7452e4bd93dc27dd98f23204b94254da42
dika aditia <aditiaaadicka@gmail.com>
2022-06-09 16:59:07 +0700

    job listing ui done

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,87 +3,57 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ListingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(Request $request)
     {
         $listings = Listing::where('is_active', true)
             ->with('tags')
             ->latest()
             ->get();
 
-        return view('listings.index', compact('listings'));
+        $tags = Tag::orderBy('name')->get();
+
+        if ($request->has('s'))
+        {
+            $query = strtolower($request->get('s'));
+            $listings = $listings->filter(function ($item) use ($query) {
+
+                if (Str::contains(strtolower($item->title), $query))
+                {
+                    return true;
+                }
+
+                if (Str::contains(strtolower($item->company), $query))
+                {
+                    return true;
+                }
+
+                if (Str::contains(strtolower($item->location), $query))
+                {
+                    return true;
+                }
+
+                return false;
+            });
+        }
+
+        if ($request->has('tag'))
+        {
+            $tag = $request->get('tag');
+            $listings = $listings->filter(function ($item) use ($tag) {
+                return $item->tags->contains('slug', $tag);
+            });
+        }
+
+        return view('listings.index', compact('listings', 'tags'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
     {
         //
     }
