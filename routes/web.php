b74366eef3ac36259dad06aa237653a1379db8be
dika aditia <aditiaaadicka@gmail.com>
2022-06-09 09:48:35 +0700

    wip frontend layout

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\ListingController::class, 'index'])->name('listings.index');
+Route::get('/listing-create', [\App\Http\Controllers\ListingController::class, 'create'])->name('listings.create');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
